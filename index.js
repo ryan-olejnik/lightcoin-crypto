5b87058887374f1121f9dee52577b18ec1b8f386
Ryan <ryan.olejnik@gmail.com>
2018-05-08 18:15:13 +0000

    added transaction list and transaction validator

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username){
+    this.username = username;
+    // this.balance = 0;
+    this.transaction_list = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  addTransaction(transaction){
+    this.transaction_list.push(transaction);
   }
 
+  get balance(){
+    let balance = 0;
+    for (let transaction of this.transaction_list){
+      balance += transaction.value;
+    }
+    return balance;
+  }
 }
 
+class Transaction{
+  constructor(account, amount){
+    this.account = account;
+    this.amount = amount;
+  }
+  commit(){
+    this.time = new Date();
+    if ((this.account.balance + this.value) >= 0){
+      this.account.addTransaction(this);
+    } else{
+      console.log('Not enough Funds!!!');
+    }
+  }
+}
 
+class Deposit extends Transaction{
+  get value(){
+    return this.amount;
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value(){
+    return -this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+let myAccount = new Account('ry-guy');
+
+let t1 = new Deposit(myAccount, 1000);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+let t2 = new Withdrawal(myAccount, 200);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+let t3 = new Withdrawal(myAccount, 700);
+t3.commit();
+
+
+console.log(myAccount.balance);
+console.log(myAccount.balance);
+
+
+
+
+
